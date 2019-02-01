a1820a2591b7aeb735dd8c3e9a8c2d70f4c8ab7b
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-02-01 13:18:04 -0800

    add icon placeholder for stock and weather

--- README.md
+++ README.md
@@ -38,10 +38,18 @@ $ ng serve
 ## Completed Features
 * Firebase Google authentication enabled for Login.
 * Weather API integration which allows the user to change the location and get the data for the location.
+* Shows the current date when on the website.
+* Horizontal navigation to direct users to NYT's content.
+
 
 ## Planned Features
-* Stock API/widget to display live feed of the S&P 500, Dow, Nasdaq.
-* Reponsive with mobile devices.
+* Use Stock API or widget for realtime data related to S&P 500, Dow, Nasdaq.
+* Display the time of published article.
+* Responsive with mobile devices.
+* Expand navigation icons to display additional navigation items.
+* Expand search functionality to allow user of search content on website.
+* Get live articles via the NYT API and integrate with the clone website.
+* Store the articles in Firebase Database to be retrieved at a later time.
 
 ## Technology
 * Javascript
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,9 +7,12 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
   title:string = "Article Title"
+  article:string ="Lorem Ipsum has been the industry's standard dummy text! "
   text: string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
 
-  item: string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  item: string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+
+  sideitem: string = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 
   imgMedium = "assets/img/Political-comedy.jpg"
 
