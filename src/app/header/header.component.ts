7b6bd8ac7a5db93d98d49e28bdffc37bc5405962
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-02-01 08:43:13 -0800

    moved firebase authentication to header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,36 @@
 import { Component} from '@angular/core';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [AuthenticationService]
 })
+
 export class HeaderComponent {
   today: number = Date.now();
+  user;
+  private isLoggedIn: Boolean;
+  private userName: String;
+
+  constructor(public authService: AuthenticationService) {
+    this.authService.user.subscribe(user =>  {
+      if (user == null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+        this.userName = user.displayName;
+      }
+    });
+  }
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 
 }
